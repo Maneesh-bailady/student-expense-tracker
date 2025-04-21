13db1e52c2dbe45a582075caf7963d8dc57c839d
Your Name <Hariharanath247@gmail.com>
2025-04-21 12:19:26 +0530

    hari

--- frontend/app/src/services/api.ts
+++ frontend/app/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://student-expense-tracker-65e7.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
